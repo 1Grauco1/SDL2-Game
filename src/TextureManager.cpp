b9a53c30c65c586fa8cdbf78c5c6a0f8a4d2fa07
g_ant <g.ant.facul@gmail.com>
2026-04-15 10:17:21 -0300

    file estructure

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,5 +1,6 @@
 #include "TextureManager.h"
 #include <SDL2/SDL_image.h>
+
 #include <iostream>
 
 SDL_Texture *TextureManager::loadTexture(const char *imgFile,
--- src/Window.cpp
+++ src/Window.cpp
@@ -1,5 +1,4 @@
 #include "Window.h"
-#include <cstdlib>
 #include <iostream>
 
 Window::Window()
